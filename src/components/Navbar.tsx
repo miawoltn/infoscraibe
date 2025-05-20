44e23d9cfefdeb3fa5ced9a83070bf77c24ef943
miawoltn <amnabdulmalik@gmail.com>
2025-05-20 19:30:16 +0100

    fix: adjust margin in Dashboard component for improved layout; remove sticky positioning in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
   const isAuth = !!userId;
 
   return (
-    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 dark:border-gray-700 bg-white/75 dark:bg-transparent backdrop-blur-lg transition-all'>
+    <nav className='h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 dark:border-gray-700 bg-white/75 dark:bg-transparent backdrop-blur-lg transition-all'>
       <MaxWidthWrapper>
         <div className='flex h-14 items-center justify-between border-b border-zinc-200 dark:border-gray-700'>
           <Link
